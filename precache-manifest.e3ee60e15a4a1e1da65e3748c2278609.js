7633994976d30f0a4629975b0b586586983a0c7b
Maria Dahlquist <maria72865@gmail.com>
2020-07-28 16:14:45 -0500

    Updates

---
+++ precache-manifest.e3ee60e15a4a1e1da65e3748c2278609.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "1908f791a0ad67cf0874",
+    "url": "/maria72865/md-react-native-memipedia.git/static/js/app.872f36da.chunk.js"
+  },
+  {
+    "revision": "942eae4b183301f5c5da",
+    "url": "/maria72865/md-react-native-memipedia.git/static/js/runtime~app.a33b970a.js"
+  },
+  {
+    "revision": "6f12664bc7bb6a82c254",
+    "url": "/maria72865/md-react-native-memipedia.git/static/js/2.23d671ea.chunk.js"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/maria72865/md-react-native-memipedia.git/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/maria72865/md-react-native-memipedia.git/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "a34e0dbc91ea34b35b434c8fd62689fb",
+    "url": "/maria72865/md-react-native-memipedia.git/static/media/memipedia-logo.8791171f.png"
+  },
+  {
+    "revision": "f3f68bf5b7dce8acae55c25f724625dd",
+    "url": "/maria72865/md-react-native-memipedia.git/expo-service-worker.js"
+  },
+  {
+    "revision": "41fc60285f041f8a8fbe1c79522ffd99",
+    "url": "/maria72865/md-react-native-memipedia.git/index.html"
+  },
+  {
+    "revision": "bf87f3d60c7e30904350a25d6e1403e5",
+    "url": "/maria72865/md-react-native-memipedia.git/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/maria72865/md-react-native-memipedia.git/serve.json"
+  },
+  {
+    "revision": "1d5e930a0beaf5006840f9af85e3e75d",
+    "url": "/maria72865/md-react-native-memipedia.git/static/js/2.23d671ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10ce0552ee7802193950fa9181bd961",
+    "url": "/maria72865/md-react-native-memipedia.git/favicon-16.png"
+  },
+  {
+    "revision": "b333dcaccf681a69a1b0a5058aa06fcb",
+    "url": "/maria72865/md-react-native-memipedia.git/favicon-32.png"
+  },
+  {
+    "revision": "62273473911e68575ace8d48f6ebd399",
+    "url": "/maria72865/md-react-native-memipedia.git/favicon.ico"
+  },
+  {
+    "revision": "512ef4f7d3fcfd86f8bce5adac8fd5c5",
+    "url": "/maria72865/md-react-native-memipedia.git/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/maria72865/md-react-native-memipedia.git/expo-service-worker.js",{scope:"/maria72865/md-react-native-memipedia.git/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
